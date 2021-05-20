bd0f8a8dc25c1ad4778be8c222b3649dc7511972
moPellowe <mmagcala@uwaterloo.ca>
2021-05-20 17:28:36 -0400

    Update user guide and configure.R script
    The user guide was updated to account for device bioavailability. These changes were made within the particle deposition implementation section and the tutorial. The "configure_inhalation_parameters.R" file was also corrected with proper values.

--- configure_inhalation_parameters.R
+++ configure_inhalation_parameters.R
@@ -7,25 +7,24 @@ source("custom_deposition.R")
 
 molecule_name <- "Ciprofloxacin"
 particle_diameters_dm <- 2.6e-5
-mean_particle_radius_dm <- 2.6e-5/2
-sd_particle_radius_dm <- 1.8e-5/2
+geomean_particle_radius_dm <- 2.6e-5/2
+gsd_particle_radius_dm <- 1.8e-5/2
 
 ########## Empirical equations ##########
 # Inhaled
 pkml_file <- "inhaled_ciprofloxacin.pkml"
 oral_bioavailability <- 0.7
-lung_bioavailability <- 0.222
+lung_bioavailability <- 0.2509824
 
-populate_model(pkml_file, molecule_name, particle_diameters_dm, mean_particle_radius_dm, sd_particle_radius_dm,
-               oral_bioavailability, lung_bioavailability)
+populate_model(pkml_file, molecule_name, particle_diameters_dm, geomean_particle_radius_dm, gsd_particle_radius_dm,
+			   oral_bioavailability, lung_bioavailability, logScale = TRUE)
 
 ########## Stass et al (2017) deposition ##########
 # Inhaled
 pkml_file <- "inhaled_ciprofloxacin.pkml"
 deposition_fractions <- matrix(c(0.394, rep(0.015875, 24)), nrow=25, ncol=1)
 oral_bioavailability <- 0.7
-# lung bioavailability is already taken into account in deposition_fractions, so the value is left at 1
-lung_bioavailability <- 1
+# Lung and device bioavailabilities are already taken into account in deposition_fractions, so the values are left at 1
 
-custom_deposition(pkml_file, molecule_name, particle_diameters_dm, mean_particle_radius_dm, sd_particle_radius_dm, 
-                  deposition_fractions, oral_bioavailability, lung_bioavailability)
+custom_deposition(pkml_file, molecule_name, particle_diameters_dm, geomean_particle_radius_dm, gsd_particle_radius_dm, 
+				  deposition_fractions, oral_bioavailability, logScale = TRUE)
